a3e640e84934c0880bbc72d73f0b9152b822a0b9
Akram <aliakramtushar@gmail.com>
2021-03-23 23:30:23 +0600

    Update CommonGateway.cs

--- MERP.Engine/GlobalGateway/CommonGateway.cs
+++ MERP.Engine/GlobalGateway/CommonGateway.cs
@@ -8,10 +8,7 @@ namespace MERP.Engine.GlobalGateway
 {
     public class CommonGateway
     {
-
         private string ConnectionString = "Server=DESKTOP-DORUQO5;Database=MERP;User ID='';password='';Integrated Security=true";  // Local Home PC
-
-
 
         public SqlConnection Connection { get; set; }
         public SqlCommand Command { get; set; }
